f7c5a3c40970785e5c4500633c7207460aeb9d13
debug <1142179118@qq.com>
2022-07-14 16:56:25 +0800

    Add ms_ssim loss and update ssim loss

--- DBWDL/dbwloss.py
+++ DBWDL/dbwloss.py
@@ -1,65 +1,174 @@
 import torch
+import sys
+import time
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from math import exp
 
+# from https://github.com/vtsynergy/2D-DECT/blob/510ce112c41bd0aff9cffee3555bf13d23a7cba7/code/train_main2_jy.py
+
 
 def gaussian(window_size, sigma):
-    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
-    return gauss / gauss.sum()
+    gauss = torch.Tensor([exp(-(x - window_size//2)**2/float(2*sigma**2)) for x in range(window_size)])
+    return gauss/gauss.sum()
 
 
-def create_window(window_size, channel):
+def create_window(window_size, channel=1):
     _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
     _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
     window = _2D_window.expand(channel, 1, window_size, window_size).contiguous()
     return window
 
+# def read_correct_image(path):
+#     offset = 0
+#     ct_org = None
+#     with Image.open(path) as img:
+#         ct_org = np.float32(np.array(img))
+#         if 270 in img.tag.keys():
+#             for item in img.tag[270][0].split("\n"):
+#                 if "c0=" in item:
+#                     loi = item.strip()
+#                     offset = re.findall(r"[-+]?\d*\.\d+|\d+", loi)
+#                     offset = (float(offset[1]))
+#     ct_org = ct_org + offset
+#     neg_val_index = ct_org < (-1024)
+#     ct_org[neg_val_index] = -1024
+#     return ct_org
 
-def _ssim(img1, img2, window, window_size, channel, size_average=True, data_range=1, K1=0.01, K2=0.03):
-    mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
-    mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=channel)
+
+def ssim(img1, img2, window_size=11, window=None, size_average=True, full=False, data_range=None):
+    # Value range can be different from 255. Other common ranges are 1 (sigmoid) and 2 (tanh).
+    if data_range is None:
+        if torch.max(img1) > 128:
+            max_val = 255
+        else:
+            max_val = 1
+
+        if torch.min(img1) < -0.001:
+            min_val = -0.1
+        else:
+            min_val = 0
+        L = max_val - min_val
+    else:
+        L = data_range
+
+    padd = 0
+    (batch, channel, height, width) = img1.size()
+    if window is None:
+        real_size = min(window_size, height, width)
+        window = create_window(real_size, channel=channel).to(img1.device)
+
+    mu1 = F.conv2d(img1, window, padding=padd, groups=channel)
+    mu2 = F.conv2d(img2, window, padding=padd, groups=channel)
 
     mu1_sq = mu1.pow(2)
     mu2_sq = mu2.pow(2)
     mu1_mu2 = mu1 * mu2
 
-    sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size // 2, groups=channel) - mu1_sq
-    sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size // 2, groups=channel) - mu2_sq
-    sigma12 = F.conv2d(img1 * img2, window, padding=window_size // 2, groups=channel) - mu1_mu2
+    sigma1_sq = F.conv2d(img1 * img1, window, padding=padd, groups=channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2 * img2, window, padding=padd, groups=channel) - mu2_sq
+    sigma12 = F.conv2d(img1 * img2, window, padding=padd, groups=channel) - mu1_mu2
 
-    C1 = (K1*data_range) ** 2
-    C2 = (K2*data_range) ** 2
+    C1 = (0.01 * L) ** 2
+    C2 = (0.03 * L) ** 2
 
-    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+    v1 = 2.0 * sigma12 + C2
+    v2 = sigma1_sq + sigma2_sq + C2
+    cs = torch.mean(v1 / v2)  # contrast sensitivity
+
+    ssim_map = ((2 * mu1_mu2 + C1) * v1) / ((mu1_sq + mu2_sq + C1) * v2)
 
     if size_average:
-        return ssim_map.mean()
+        ret = ssim_map.mean()
     else:
-        return ssim_map.mean(1).mean(1).mean(1)
+        ret = ssim_map.mean(1).mean(1).mean(1)
+
+    if full:
+        return ret, cs
+    return ret
 
 
+def msssim(img1, img2, window_size=11, size_average=True, data_range=None, normalize=None):
+    device = img1.device
+    weights = torch.FloatTensor([0.0448, 0.2856, 0.3001, 0.2363, 0.1333]).to(device)
+    levels = weights.size()[0]
+    ssims = []
+    mcs = []
+    for _ in range(levels):
+        sim, cs = ssim(img1, img2, window_size=window_size, size_average=size_average, full=True, data_range=data_range)
+
+        # Relu normalize (not compliant with original definition)
+        if normalize == "relu":
+            ssims.append(torch.relu(sim))
+            mcs.append(torch.relu(cs))
+        else:
+            ssims.append(sim)
+            mcs.append(cs)
+
+        img1 = F.avg_pool2d(img1, (2, 2))
+        img2 = F.avg_pool2d(img2, (2, 2))
+
+    ssims = torch.stack(ssims)
+    mcs = torch.stack(mcs)
+
+    # Simple normalize (not compliant with original definition)
+    # TODO: remove support for normalize == True (kept for backward support)
+    if normalize == "simple" or normalize == True:
+        ssims = (ssims + 1) / 2
+        mcs = (mcs + 1) / 2
+
+    pow1 = mcs ** weights
+    pow2 = ssims ** weights
+
+
+    # From Matlab implementation https://ece.uwaterloo.ca/~z70wang/research/iwssim/
+    output = torch.prod(pow1[:-1] * pow2[-1])
+    if(torch.isnan(output)):
+        print(pow1)
+        print(pow2)
+        print(ssims)
+        print(mcs)
+        exit()
+
+
+    return output
+
+
+# Classes to re-use window
 class SSIM(torch.nn.Module):
-    def __init__(self, window_size=11, size_average=True):
+    def __init__(self, window_size=11, size_average=True, data_range=None):
         super(SSIM, self).__init__()
         self.window_size = window_size
         self.size_average = size_average
-        self.channel = 1
-        self.window = create_window(window_size, self.channel)
+        self.data_range = data_range
 
-    def forward(self, img1, img2, data_range=1, K1=0.01, K2=0.03):
+        # Assume 1 channel for SSIM
+        self.channel = 1
+        self.window = create_window(window_size)
+
+    def forward(self, img1, img2, data_range=None):
         (_, channel, _, _) = img1.size()
 
-        if channel == self.channel and self.window.data.type() == img1.data.type():
-            window = self.window
+        if channel == self.channel and self.window.dtype == img1.dtype:
+            window = self.window.to(img1.device)
         else:
-            window = create_window(self.window_size, channel)
-
-            if img1.is_cuda:
-                window = window.cuda(img1.get_device())
-            window = window.type_as(img1)
-
-            self.window = window
+            window = create_window(self.window_size, channel).to(img1.device).type(img1.dtype)
+            self.window = window.to(img1.device)
             self.channel = channel
 
-        return _ssim(img1, img2, window, self.window_size, channel, self.size_average,
-                     data_range=data_range, K1=K1, K2=K2)
+        return ssim(img1, img2, window=window, window_size=self.window_size, size_average=self.size_average,
+                    data_range=data_range)
+
+class MSSSIM(torch.nn.Module):
+    def __init__(self, window_size=11, size_average=True, channel=3):
+        super(MSSSIM, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = channel
+
+    def forward(self, img1, img2, data_range=None):
+        # TODO: store window between calls if possible
+        return msssim(img1, img2, window_size=self.window_size, size_average=self.size_average,
+                      data_range=data_range,normalize="simple")
+        #return msssim(img1, img2, window_size=self.window_size, size_average=self.size_average)
